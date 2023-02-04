c530282bbd0116a827ff0c89739ad347c1c9f095
오태현 <jeffrey@kai-i.com>
2023-02-04 19:48:58 +0900

    waitForWebToons

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,18 +3,22 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:toonflix/models/webtoon_model.dart';
 
+// class 내에 state 값이 없기 때문에 static 활용
 class ApiService {
-  final String baseUrl = 'https://webtoon-crawler.nomadcoders.workers.dev';
-  final String today = "today";
+  static const String baseUrl =
+      'https://webtoon-crawler.nomadcoders.workers.dev';
+  static const String today = "today";
 
   // 비동기를 처리하기 위해서는 async 를 사용하며
   // 요청하는 곳 앞에 await 를 적용
-  Future<List<WebToonModel>> getTodaysToons() async {
+  // return type 은 Future 로 감싸야한다
+  static Future<List<WebToonModel>> getTodaysToons() async {
     List<WebToonModel> webToonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
+      // jsonDecode 리턴 타입이 dynamic 이기 때문에 List<dynamic> 으로 받는다`
       final List<dynamic> webtoons = jsonDecode(response.body);
       for (var webtoon in webtoons) {
         webToonInstances.add(WebToonModel.fromjson(webtoon));
