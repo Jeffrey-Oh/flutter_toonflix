cd8b5d0fcd5090eef8a9e59840f6c2e104007c36
오태현 <jeffrey@kai-i.com>
2023-02-04 19:59:20 +0900

    FutureBuilder

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,33 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:toonflix/models/webtoon_model.dart';
 import 'package:toonflix/services/api_service.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<WebToonModel> webtoons = [];
-  bool isLoading = true;
-
-  void waitForWebtoons() async {
-    webtoons = await ApiService.getTodaysToons();
-    isLoading = false;
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    waitForWebtoons();
-  }
+  final Future<List<WebToonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
-    print(webtoons);
-    print(isLoading);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -41,6 +21,17 @@ class _HomeScreenState extends State<HomeScreen> {
             fontSize: 26,
           ),
         ),
+      ),
+      body: FutureBuilder(
+        // FutureBuilder : Future 타입의 값을 받아주는 위젯으로 async 나 await 처리를 직접할 필요 없음
+        // 위젯에서 다 해줌 (위젯 안에 있겠지 당연히)
+        future: webtoons,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const Text('There is Data!');
+          }
+          return const Text('Loading !');
+        },
       ),
     );
   }
