91def21f6a9b5d0b6173dc7198e035647f7bdad6
오태현 <jeffrey@kai-i.com>
2023-02-05 22:47:36 +0900

    Add Toon, Episode Fetching Method

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_detail_model.dart';
+import 'package:toonflix/models/webtoon_episode_model.dart';
 import 'package:toonflix/models/webtoon_model.dart';
 
 // class 내에 state 값이 없기 때문에 static 활용
@@ -24,6 +26,35 @@ class ApiService {
         webToonInstances.add(WebToonModel.fromjson(webtoon));
       }
       return webToonInstances;
+    }
+
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse('$baseUrl/$id');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final toon = jsonDecode(response.body);
+      return toon;
+    }
+
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getEpisodeById(String id) async {
+    List<WebtoonEpisodeModel> episodesInstances = [];
+    final url = Uri.parse('$baseUrl/$id/episodes');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        episodesInstances.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+
+      return episodesInstances;
     }
 
     throw Error();
